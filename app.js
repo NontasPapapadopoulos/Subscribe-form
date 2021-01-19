e6941784085e47a4b89c6fbd834733f0f072c77a
Nontas Papadopoulos <59374756+NontasPapapadopoulos@users.noreply.github.com>
2021-01-20 01:33:45 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+
+const app = express();
+
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/signup.html");
+});
+
+
+app.post('/', function (req, res) {
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+
+            }
+        ]
+    };
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us7.api.mailchimp.com/3.0/lists/f0d10f884f";
+
+    const options = {
+        method: "POST",
+        auth: "nondas:bd0a47a4bf28932b39bec9c501b5f7a4-us7"
+    }
+
+    const request = https.request(url, options, function (response) {
+
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html")
+        } else {
+            res.sendFile(__dirname + "/failure.html")
+        }
+
+        response.on("data", function (data) {
+            console.log(JSON.parse(data));
+        })
+    })
+
+    request.write(jsonData);
+    request.end();
+    console.log(firstName, lastName, email);
+});
+
+app.post("/failure", function (req, res) {
+    res.redirect("/");
+});
+
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log("The server is running at port 3000.")
+})
+
+
+//bd0a47a4bf28932b39bec9c501b5f7a4-us7   server = us7 
+
+
+
+//f0d10f884f
